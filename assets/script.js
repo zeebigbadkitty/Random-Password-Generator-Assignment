f05be0abffe8779fb2f20ae54e47ca38ec9f2830
Socially Awkward Streams <candice.radam@gmail.com>
2022-09-29 10:38:28 -0500

    First function on lines 17-20

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+
+var characters= [] ///"!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"/
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +14,32 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+function generatePassword(){
+  var passwordLength = parseInt(prompt("Enter password length?"))
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
+    return "Please enter a valid number."
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// 
+//GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+//(How many characters?)(Special Characters?)(Capital Letters?)(Lower case?)(Numbers?)
+//"!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"/
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page//
